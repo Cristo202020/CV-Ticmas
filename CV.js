69ef9ed2a25a2837d1612c2c0ad890783b5135c0
Cristo <Cristian-toledo@hotmail.com.ar>
2023-02-05 13:03:03 -0300

    Primer commit

---
+++ CV.js
@@ -0,0 +1,35 @@
+    function Mostrar(){
+        document.getElementById('Experiencia').style.display = 'block';
+        document.getElementById('Experiencia2').style.display = 'block';
+    }
+
+    function Ocultar(){
+        document.getElementById('Experiencia').style.display = 'none';
+        document.getElementById('Experiencia2').style.display = 'none';
+    }
+
+    function Mostrar2 (){
+        document.getElementById('Educación').style.display = 'block';
+        document.getElementById('Educación2').style.display = 'block';
+    }
+
+    function Ocultar2(){
+        document.getElementById('Educación').style.display = 'none';
+        document.getElementById('Educación2').style.display = 'none';
+    }
+
+    function Mostrar3(){
+        document.getElementById('Habilidades').style.display = 'block';
+    }
+
+    function Ocultar3(){
+        document.getElementById('Habilidades').style.display = 'none';
+    }
+
+    function Mostrar4(){
+        document.getElementById('Idioma').style.display = 'block';
+    }
+
+    function Ocultar4(){
+        document.getElementById('Idioma').style.display = 'none';
+    }
